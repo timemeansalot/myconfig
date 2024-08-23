690e21c37a7ea62c6fa88f8b357025192f45e52b
timemeansalot <timemeansalot@gmail.com>
2024-08-23 15:32:58 +0800

    change to add function

--- config.go
+++ config.go
@@ -2,15 +2,9 @@ package config
 
 import (
 	"fmt"
-	"github.com/spf13/viper"
 )
 
-func ReadConfig(configFilePath string) error {
-	// Initialize a new viper configuration object
-	viper.SetConfigFile(configFilePath)
-	err := viper.ReadInConfig()
-	if err != nil {
-		return fmt.Errorf("error reading config file: %s", err)
-	}
-	return nil
+func add(a int64, b int64) int64 {
+	fmt.Println("do addtion")
+	return a + b
 }
